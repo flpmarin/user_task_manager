e37ce962af18f44401ccc010f80850773cbb66db
felipe_marin_st <flpmarin@digitechfp.com>
2024-10-23 01:39:52 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\WebController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WebController::class, 'index']);
+Route::middleware(['auth:sanctum'])->get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('example',[DashboardController::class, 'example'])->name('example');
